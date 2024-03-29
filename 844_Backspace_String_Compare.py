bbd999ff287c089929107116d8066c936ecd55d0
TakayaShirai <136686674+TakayaShirai@users.noreply.github.com>
2024-03-29 19:29:15 +0900

    Add 844_Backspace_String_Compare

--- 844_Backspace_String_Compare.py
+++ 844_Backspace_String_Compare.py
@@ -1,20 +1,60 @@
 class Solution:
     def backspaceCompare(self, s: str, t: str) -> bool:
-        stackS = deque()
-        stackT = deque()
+        pointerS = len(s) - 1
+        pointerT = len(t) - 1
         
-        for char in s:
-            if char == "#":
-                if stackS:
-                    stackS.pop()
+        while pointerS >= 0 or pointerT >= 0:
+            if s[pointerS] == "#" or t[pointerT] == "#":
+                if s[pointerS] == "#":
+                    backCount = 2
+                    while backCount > 0:
+                        backCount -= 1
+                        pointerS -= 1
+                        if pointerS < 0:
+                            break
+                        if s[pointerS] == "#":
+                            backCount += 2
+                if t[pointerT] == "#":
+                    backCount = 2
+                    while backCount > 0:
+                        backCount -= 1
+                        pointerT -= 1
+                        if pointerT < 0:
+                            break
+                        if t[pointerT] == "#":
+                            backCount += 2
+            
             else:
-                stackS.append(char)
+                if pointerS < 0 or pointerT < 0:
+                    if pointerS < 0 and pointerT < 0:
+                        return True
+                    else:
+                        return False
+            
+                else:
+                    if s[pointerS] != t[pointerT]:
+                        return False
+                    pointerS -= 1
+                    pointerT -= 1
         
-        for char in t:
-            if char == "#":
-                if stackT:
-                    stackT.pop()
-            else:
-                stackT.append(char)
+        return True
+                
+#         stackS = deque()
+#         stackT = deque()
+        
+#         for char in s:
+#             if char == "#":
+#                 if stackS:
+#                     stackS.pop()
+#             else:
+#                 stackS.append(char)
+        
+#         for char in t:
+#             if char == "#":
+#                 if stackT:
+#                     stackT.pop()
+#             else:
+#                 stackT.append(char)
 
-        return stackS == stackT
+#         return stackS == stackT
+        
