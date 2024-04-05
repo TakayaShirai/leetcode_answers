4dc640d2e48b59ff8745e8d187800e24d26269cc
TakayaShirai <136686674+TakayaShirai@users.noreply.github.com>
2024-04-05 16:16:13 +0900

    Add 20_Valid_Parentheses

--- 20_Valid_Parentheses.py
+++ 20_Valid_Parentheses.py
@@ -1,17 +1,33 @@
 class Solution:
     def isValid(self, s: str) -> bool:
+        hashMap = {')': '(', '}': '{', ']': '['}
         stack = deque()
         
-        for i in range(len(s)):
-            if s[i] == "(" or s[i] == "{" or s[i] == "[":
-                stack.appendleft(s[i])
-            elif s[i] == ")" and len(stack) > 0 and stack[0] == "(":
-                stack.popleft()
-            elif s[i] == "}" and len(stack) > 0 and stack[0] == "{":
-                stack.popleft()
-            elif s[i] == "]" and len(stack) > 0 and stack[0] == "[":
-                stack.popleft()
+        for char in s:
+            if char in hashMap:
+                if stack:
+                    if stack.pop() != hashMap[char]:
+                        return False
+                else:
+                    return False
             else:
-                return False
+                stack.append(char)
         
-        return len(stack) == 0
+        return not stack
+        
+        
+        # stack = deque()
+        
+        # for i in range(len(s)):
+        #     if s[i] == "(" or s[i] == "{" or s[i] == "[":
+        #         stack.appendleft(s[i])
+        #     elif s[i] == ")" and len(stack) > 0 and stack[0] == "(":
+        #         stack.popleft()
+        #     elif s[i] == "}" and len(stack) > 0 and stack[0] == "{":
+        #         stack.popleft()
+        #     elif s[i] == "]" and len(stack) > 0 and stack[0] == "[":
+        #         stack.popleft()
+        #     else:
+        #         return False
+        
+        # return len(stack) == 0
