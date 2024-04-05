d8274b794f5c0b57db6d9c18645309bd76c9405f
TakayaShirai <136686674+TakayaShirai@users.noreply.github.com>
2024-04-05 14:45:16 +0900

    Add 430_Flatten_a_Multilevel_Doubly_Linked_List

--- 430_Flatten_a_Multilevel_Doubly_Linked_List.py
+++ 430_Flatten_a_Multilevel_Doubly_Linked_List.py
@@ -9,32 +9,59 @@ class Node:
 """
 
 class Solution:
+# Iterative Approach
+# T: O(N)
+# S: O(1)
     def flatten(self, head: 'Optional[Node]') -> 'Optional[Node]':
         cur = head
-        stack = deque()
         
         while cur:
             if cur.child:
-                self.subFlatten(cur, stack)
+                childNode = cur.child
+                while childNode.next:
+                    childNode = childNode.next
+                
+                childNode.next = cur.next
+                if cur.next:
+                    cur.next.prev = childNode
+                
+                cur.next = cur.child
+                cur.child.prev = cur
+                
+                cur.child = None
+            
             cur = cur.next
         
         return head
         
+# Recursive Approach
+# T: O(N)
+# S: O(N)
+#     def flatten(self, head: 'Optional[Node]') -> 'Optional[Node]':
+#         cur = head
+#         stack = deque()
+        
+#         while cur:
+#             if cur.child:
+#                 self.subFlatten(cur, stack)
+#             cur = cur.next
+        
+#         return head
+        
     
-    def subFlatten(self, cur: 'Optional[Node]', stack: List['Optional[Node]']):
-        if cur.next:
-            stack.append(cur.next)
-        cur.next = cur.child
-        cur.child.prev = cur
-        cur.child = None
+#     def subFlatten(self, cur: 'Optional[Node]', stack: List['Optional[Node]']):
+#         if cur.next:
+#             stack.append(cur.next)
+#         cur.next = cur.child
+#         cur.child.prev = cur
+#         cur.child = None
         
-        while cur.next:
-            cur = cur.next
-            if cur.child:
-                self.subFlatten(cur, stack)
+#         while cur.next:
+#             cur = cur.next
+#             if cur.child:
+#                 self.subFlatten(cur, stack)
         
-        if stack:
-            cur.next = stack.pop()
-            cur.next.prev = cur
-            cur = cur.next
-        
+#         if stack:
+#             cur.next = stack.pop()
+#             cur.next.prev = cur
+#             cur = cur.next
