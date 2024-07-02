a4bcd52aa1788a27b3898bc6d9b8ab0c97933fd8
TakayaShirai <s78taka03@gmail.com>
2024-07-03 00:12:19 +0900

    [swift] Add 283. Move Zeroes.

---
+++ swift/283_move_zeroes.swift
@@ -0,0 +1,15 @@
+class Solution {
+    func moveZeroes(_ nums: inout [Int]) {
+        var left: Int = 0
+        
+        for right in 0..<nums.count {
+            if nums[right] != 0 {
+                var tmp = nums[left]
+                nums[left] = nums[right]
+                nums[right] = tmp
+                left += 1
+            }
+        }
+    }
+}
+
