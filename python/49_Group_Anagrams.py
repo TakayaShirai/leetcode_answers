4b6b83f2144c41de611bf66571bc11c0aa18c1ae
TakayaShirai <s78taka03@gmail.com>
2024-07-04 23:52:59 +0900

    [python] Modify 49. Group Anagrams.

--- python/49_Group_Anagrams.py
+++ python/49_Group_Anagrams.py
@@ -1,11 +1,27 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        mp = {}
+        anagrams = {}
         
-        for word in strs:
-            sorted_word = ''.join(sorted(word))
-            if sorted_word not in mp:
-                mp[sorted_word] = []
-            mp[sorted_word].append(word)
+        for str in strs:
+            counts = [0] * 26
+            
+            for char in str:
+                counts[ord(char) - ord("a")] += 1
+            
+            if tuple(counts) in anagrams:
+                anagrams[tuple(counts)].append(str)
+            else:
+                anagrams[tuple(counts)] = [str]
         
-        return list(mp.values())
+        return anagrams.values()
+        
+        # mp = {}
+        
+        # for word in strs:
+        #     sorted_word = ''.join(sorted(word))
+        #     if sorted_word not in mp:
+        #         mp[sorted_word] = []
+        #     mp[sorted_word].append(word)
+        
+        # return list(mp.values())
+        
