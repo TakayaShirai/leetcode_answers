8bc65ef39b3461433fcf2552373b44d3c4e42471
TakayaShirai <s78taka03@gmail.com>
2024-07-05 23:30:41 +0900

    [swift] Modify 49. Group Anagrams.

---
+++ swift/49_group_anagrams.swift
@@ -0,0 +1,18 @@
+class Solution {
+    func groupAnagrams(_ strs: [String]) -> [[String]] {
+        var anagrams: [[Character : Int] : [String]] = [:]
+        
+        for str in strs {
+            var count: [Character : Int] = [:]
+            
+            for char in str {
+                count[char, default:0] += 1
+            }
+            
+            anagrams[count, default:[]].append(str)
+        }
+        
+        return Array(anagrams.values)
+    }
+}
+
