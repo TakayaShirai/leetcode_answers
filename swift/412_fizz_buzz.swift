5fa49f191a78f8dc8a26b5c16c4c45f5f0a4bf0a
TakayaShirai <s78taka03@gmail.com>
2024-07-05 23:06:17 +0900

    [swift] Add 412. Fizz Buzz.

---
+++ swift/412_fizz_buzz.swift
@@ -0,0 +1,25 @@
+class Solution {
+    func fizzBuzz(_ n: Int) -> [String] {
+        var res: [String] = []
+        
+        for i in 1...n {
+            var ans: String = ""
+            
+            if i % 3 == 0 {
+                ans = "Fizz"
+                if i % 5 == 0 {
+                    ans += "Buzz"
+                }
+            } else if i % 5 == 0 {
+                ans = "Buzz"
+            } else {
+                ans = String(i)
+            }
+            
+            res.append(ans)
+        }
+        
+        return res
+    }
+}
+
