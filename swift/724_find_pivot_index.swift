a42763f0203c5e17fde7ca01727e587eaf697f1d
TakayaShirai <s78taka03@gmail.com>
2024-07-12 10:10:10 +0900

    [swift] Add 724. Find Pivot Index.

---
+++ swift/724_find_pivot_index.swift
@@ -0,0 +1,25 @@
+class Solution {
+    func pivotIndex(_ nums: [Int]) -> Int {
+        var total: Int = 0
+        
+        for num in nums {
+            total += num
+        }
+        
+        var leftSum: Int = 0
+        var rightSum: Int = 0
+        
+        for i in 0..<nums.count {
+            rightSum = total - leftSum - nums[i]
+            
+            if leftSum == rightSum {
+                return i
+            }
+            
+            leftSum += nums[i]
+        }
+        
+        return -1
+    }
+}
+
