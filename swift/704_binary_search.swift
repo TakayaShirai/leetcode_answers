38b062202a99a538be9b2bf73de68aad97fa5363
TakayaShirai <s78taka03@gmail.com>
2024-07-19 23:28:33 +0900

    [swift] Add 704. Binary Search.

---
+++ swift/704_binary_search.swift
@@ -0,0 +1,22 @@
+class Solution {
+    func search(_ nums: [Int], _ target: Int) -> Int {
+        var left: Int = 0
+        var right: Int = nums.count - 1
+        var mid: Int = 0
+        
+        while left <= right {
+            mid = (left + right) / 2
+            
+            if nums[mid] == target {
+                return mid
+            } else if nums[mid] > target {
+                right = mid - 1 
+            } else {
+                left = mid + 1
+            }
+        }
+        
+        return -1
+    }
+}
+
