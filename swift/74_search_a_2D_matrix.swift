b997f0f67097010aa117389b5f3eab94d6d84bb3
TakayaShirai <s78taka03@gmail.com>
2024-07-21 15:11:08 +0900

    [swift] Add 74. Search a 2D Matrix.

---
+++ swift/74_search_a_2D_matrix.swift
@@ -0,0 +1,43 @@
+class Solution {
+    func searchMatrix(_ matrix: [[Int]], _ target: Int) -> Bool {
+        var left: Int = 0
+        var right: Int = matrix.count - 1
+        var mid: Int = 0
+        
+        while left <= right {
+            mid = (left + right) / 2
+            
+            if matrix[mid][0] == target {
+                return true
+            } else if matrix[mid][0] > target {
+                right = mid - 1
+            } else {
+                left = mid + 1
+            }
+        } 
+        
+        if left == 0 {
+            return false
+        }
+        
+        var row: Int = left - 1
+        left = 0
+        right = matrix[0].count - 1
+        
+        while left <= right {
+            mid = (left + right) / 2
+            
+            if matrix[row][mid] == target {
+                return true
+            } else if matrix[row][mid] > target {
+                right = mid - 1
+            } else {
+                left = mid + 1
+            }
+        }
+        
+        return false
+        
+    }
+}
+
