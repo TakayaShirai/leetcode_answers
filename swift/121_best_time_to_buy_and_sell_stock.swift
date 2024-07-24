b33df521945199aaa9b6a3cc1c93fd63b7e3be67
TakayaShirai <s78taka03@gmail.com>
2024-07-24 11:31:23 +0900

    [swift] Add 121. Best Time to Buy and Sell Stock.

---
+++ swift/121_best_time_to_buy_and_sell_stock.swift
@@ -0,0 +1,40 @@
+class Solution {
+    func maxProfit(_ prices: [Int]) -> Int {
+        var maxProfit: Int = 0
+        var left: Int = 0
+        var right: Int = 0
+        
+        while right < prices.count {
+            if prices[left] < prices[right] {
+                maxProfit = max(maxProfit, prices[right] - prices[left])
+                right += 1
+            } else {
+                left = right
+                right = left + 1
+            }
+        }
+        
+        return maxProfit
+    }
+}
+
+class Solution {
+    func maxProfit(_ prices: [Int]) -> Int {
+        var maxProfit: Int = 0
+        var left: Int = 0
+        var right: Int = 0
+        
+        while right < prices.count {
+            if prices[left] < prices[right] {
+                maxProfit = max(maxProfit, prices[right] - prices[left])
+                right += 1
+            } else {
+                left = right
+                right = left + 1
+            }
+        }
+        
+        return maxProfit
+    }
+}
+
