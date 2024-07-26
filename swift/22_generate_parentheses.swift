bbf245ae041e79b97ef05aaf06cc44b6ac1c6860
TakayaShirai <s78taka03@gmail.com>
2024-07-26 23:14:36 +0900

    [swift] Add 22. Generate Parentheses.

---
+++ swift/22_generate_parentheses.swift
@@ -0,0 +1,25 @@
+class Solution {
+    func generateParenthesis(_ n: Int) -> [String] {
+        var res: [String] = []
+        
+        func backtrack(_ openCount: Int, _ closedCount: Int, _ comb: String) {
+            if openCount == n && closedCount == n {
+                res.append(comb)
+                return
+            }
+            
+            if openCount < n {
+                backtrack(openCount + 1, closedCount, comb + "(")
+            }
+            
+            if openCount > closedCount {
+                backtrack(openCount, closedCount + 1, comb + ")")
+            }
+        } 
+        
+        backtrack(0, 0, "")
+        
+        return res
+    }
+}
+
