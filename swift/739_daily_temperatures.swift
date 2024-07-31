24bc0d641755d3f82d29156266a7e5b6071a98d9
TakayaShirai <s78taka03@gmail.com>
2024-07-31 11:17:07 +0900

    [swift] Add 739. Daily Temperatures.

---
+++ swift/739_daily_temperatures.swift
@@ -0,0 +1,18 @@
+class Solution {
+    func dailyTemperatures(_ temperatures: [Int]) -> [Int] {
+        var monDecStack: [(Int, Int)] = []
+        var res: [Int] = Array(repeating: 0, count: temperatures.count)
+        
+        for (i, temperature) in temperatures.enumerated() {
+            while !monDecStack.isEmpty && monDecStack.last!.0 < temperature {
+                let cur = monDecStack.popLast()!
+                res[cur.1] = i - cur.1
+            }
+            
+            monDecStack.append((temperature, i))
+        }
+        
+        return res
+    }
+}
+
