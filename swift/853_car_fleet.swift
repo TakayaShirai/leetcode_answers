44ee822280511a0696c17f7b247c6134cfcea6b3
TakayaShirai <s78taka03@gmail.com>
2024-07-31 16:22:27 +0900

    [swift] Add 853. Car Fleet.

---
+++ swift/853_car_fleet.swift
@@ -0,0 +1,19 @@
+class Solution {
+    func carFleet(_ target: Int, _ position: [Int], _ speed: [Int]) -> Int {
+        let array = zip(position, speed).sorted{$0.0 < $1.0}
+        var stack: [Double] = []
+        
+        for (position, speed) in array {
+            let time = Double(target - position) / Double(speed)
+            
+            while !stack.isEmpty && time >= stack.last! {
+                stack.removeLast()
+            }
+            
+            stack.append(time)
+        }
+        
+        return stack.count
+    }
+}
+
