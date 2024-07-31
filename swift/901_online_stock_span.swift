26ae317fbf0d14d1eaa1bf36804954b11f58451d
TakayaShirai <s78taka03@gmail.com>
2024-07-31 14:17:48 +0900

    [swift] Add 901. Online Stock Span.

---
+++ swift/901_online_stock_span.swift
@@ -0,0 +1,27 @@
+class StockSpanner {
+    var monDecStack: [(Int, Int)]
+
+    init() {
+        self.monDecStack = []
+    }
+    
+    func next(_ price: Int) -> Int {
+        var res = 1
+        
+        while !monDecStack.isEmpty && price >= monDecStack.last!.0 {
+            let cur = monDecStack.popLast()!
+            res += cur.1
+        }
+        
+        monDecStack.append((price, res))
+        
+        return res
+    }
+}
+
+/**
+ * Your StockSpanner object will be instantiated and called as such:
+ * let obj = StockSpanner()
+ * let ret_1: Int = obj.next(price)
+ */
+
