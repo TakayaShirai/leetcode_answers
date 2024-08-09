c23d0c3aacbedf9e2d93db8a3c80b81679904c4f
TakayaShirai <s78taka03@gmail.com>
2024-08-09 14:39:47 +0900

    [swift] Add 138. Copy List with Random Pointer.

---
+++ swift/138_copy_list_with_random_pointer.swift
@@ -0,0 +1,49 @@
+/**
+ * Definition for a Node.
+ * public class Node {
+ *     public var val: Int
+ *     public var next: Node?
+ *     public var random: Node?
+ *     public init(_ val: Int) {
+ *         self.val = val
+ *         self.next = nil
+ *         self.random = nil
+ *     }
+ * }
+ */
+
+class Solution {
+    func copyRandomList(_ head: Node?) -> Node? {   
+        guard let head = head else { return nil }
+        
+        var copyNodes: [Node: Node] = [:]
+        var cur: Node? = head
+
+        while cur != nil {
+            copyNodes[cur!] = Node(cur!.val)
+            cur = cur!.next
+        }
+        
+        cur = head
+        
+        while cur != nil {
+            if let next = cur!.next {
+                copyNodes[cur!]!.next = copyNodes[next]
+            } else {
+                copyNodes[cur!]!.next = nil
+            }
+            
+            
+            if let random = cur!.random {
+                copyNodes[cur!]!.random = copyNodes[random]
+            } else {
+                copyNodes[cur!]!.random = nil
+            }
+            
+            cur = cur!.next
+        }
+        
+        return copyNodes[head]
+    }
+}
+
