f06543450377efa2d1f26a21dbb91b15b7610351
TakayaShirai <s78taka03@gmail.com>
2024-08-23 15:56:48 +0900

    [swift] Add 108. Convert Sorted Array to Binary Search Tree.

---
+++ swift/108_convert_sorted_array_to_binary_search_tree.swift
@@ -0,0 +1,32 @@
+/// Definition for a binary tree node.
+/// public class TreeNode {
+///     public var val: Int
+///     public var left: TreeNode?
+///     public var right: TreeNode?
+///     public init() { self.val = 0; self.left = nil; self.right = nil; }
+///     public init(_ val: Int) { self.val = val; self.left = nil; self.right = nil; }
+///     public init(_ val: Int, _ left: TreeNode?, _ right: TreeNode?) {
+///         self.val = val
+///         self.left = left
+///         self.right = right
+///     }
+/// }
+class Solution {
+  func sortedArrayToBST(_ nums: [Int]) -> TreeNode? {
+
+    func helper(_ left: Int, _ right: Int) -> TreeNode? {
+      if left > right {
+        return nil
+      }
+
+      let mid = (left + right) / 2
+      let root = TreeNode(nums[mid])
+      root.left = helper(left, mid - 1)
+      root.right = helper(mid + 1, right)
+
+      return root
+    }
+
+    return helper(0, nums.count - 1)
+  }
+}
