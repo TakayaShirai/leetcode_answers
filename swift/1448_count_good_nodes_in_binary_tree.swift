8c66bd2f4eb13f1d0fd1105894e7e38cb7636c28
TakayaShirai <s78taka03@gmail.com>
2024-08-30 01:22:12 +0900

    [swift] Fix format.

--- swift/1448_count_good_nodes_in_binary_tree.swift
+++ swift/1448_count_good_nodes_in_binary_tree.swift
@@ -1,39 +1,36 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     public var val: Int
- *     public var left: TreeNode?
- *     public var right: TreeNode?
- *     public init() { self.val = 0; self.left = nil; self.right = nil; }
- *     public init(_ val: Int) { self.val = val; self.left = nil; self.right = nil; }
- *     public init(_ val: Int, _ left: TreeNode?, _ right: TreeNode?) {
- *         self.val = val
- *         self.left = left
- *         self.right = right
- *     }
- * }
- */
+/// Definition for a binary tree node.
+/// public class TreeNode {
+///     public var val: Int
+///     public var left: TreeNode?
+///     public var right: TreeNode?
+///     public init() { self.val = 0; self.left = nil; self.right = nil; }
+///     public init(_ val: Int) { self.val = val; self.left = nil; self.right = nil; }
+///     public init(_ val: Int, _ left: TreeNode?, _ right: TreeNode?) {
+///         self.val = val
+///         self.left = left
+///         self.right = right
+///     }
+/// }
 class Solution {
-    func goodNodes(_ root: TreeNode?) -> Int {
-        guard let root = root else { return 0 }
-        
-        var res: Int = 0
-        dfs(root, root.val)
-        
-        return res
-        
-        func dfs(_ node: TreeNode?, _ maxVal: Int) {
-            guard let node = node else { return }
-            var val = maxVal
-            
-            if node.val >= val {
-                res += 1
-                val = node.val
-            }
-            
-            dfs(node.left, val)
-            dfs(node.right, val)
-        }
-    }
-}
+  func goodNodes(_ root: TreeNode?) -> Int {
+    guard let root = root else { return 0 }
 
+    var res: Int = 0
+    dfs(root, root.val)
+
+    return res
+
+    func dfs(_ node: TreeNode?, _ maxVal: Int) {
+      guard let node = node else { return }
+      var val = maxVal
+
+      if node.val >= val {
+        res += 1
+        val = node.val
+      }
+
+      dfs(node.left, val)
+      dfs(node.right, val)
+    }
+  }
+}
--- swift/98_validate_binary_search_tree.swift
+++ swift/98_validate_binary_search_tree.swift
@@ -1,31 +1,28 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     public var val: Int
- *     public var left: TreeNode?
- *     public var right: TreeNode?
- *     public init() { self.val = 0; self.left = nil; self.right = nil; }
- *     public init(_ val: Int) { self.val = val; self.left = nil; self.right = nil; }
- *     public init(_ val: Int, _ left: TreeNode?, _ right: TreeNode?) {
- *         self.val = val
- *         self.left = left
- *         self.right = right
- *     }
- * }
- */
+/// Definition for a binary tree node.
+/// public class TreeNode {
+///     public var val: Int
+///     public var left: TreeNode?
+///     public var right: TreeNode?
+///     public init() { self.val = 0; self.left = nil; self.right = nil; }
+///     public init(_ val: Int) { self.val = val; self.left = nil; self.right = nil; }
+///     public init(_ val: Int, _ left: TreeNode?, _ right: TreeNode?) {
+///         self.val = val
+///         self.left = left
+///         self.right = right
+///     }
+/// }
 class Solution {
-    func isValidBST(_ root: TreeNode?) -> Bool {
-        return dfs(root, Int.min, Int.max)
-    }
-    
-    private func dfs(_ node: TreeNode?, _ leftBoundary: Int, _ rightBoundary: Int) -> Bool {
-        guard let node = node else { return true }
-        
-        if !(leftBoundary < node.val && node.val < rightBoundary) {
-            return false
-        }
+  func isValidBST(_ root: TreeNode?) -> Bool {
+    return dfs(root, Int.min, Int.max)
+  }
 
-        return dfs(node.left, leftBoundary, node.val) && dfs(node.right, node.val, rightBoundary)
+  private func dfs(_ node: TreeNode?, _ leftBoundary: Int, _ rightBoundary: Int) -> Bool {
+    guard let node = node else { return true }
+
+    if !(leftBoundary < node.val && node.val < rightBoundary) {
+      return false
     }
+
+    return dfs(node.left, leftBoundary, node.val) && dfs(node.right, node.val, rightBoundary)
+  }
 }
-
