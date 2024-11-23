abb47b81d412228c141d1df97f925552a944cb38
TakayaShirai <s78taka03@gmail.com>
2024-11-23 00:54:34 -0800

    [swift] Update 1652. Defuse the Bomb.

--- swift/1652_defuse_the_bomb.swift
+++ swift/1652_defuse_the_bomb.swift
@@ -1,46 +1,61 @@
 class Solution {
   func decrypt(_ code: [Int], _ k: Int) -> [Int] {
+    if k == 0 {
+      return Array(repeating: 0, count: code.count)
+    } else if k > 0 {
+      return posDecrypt(code, k)
+    } else {
+      return negDecrypt(code, k)
+    }
+  }
+
+  private func posDecrypt(_ code: [Int], _ k: Int) -> [Int] {
     let codeLen: Int = code.count
     var decryptedCode: [Int] = Array(repeating: 0, count: codeLen)
 
-    if k == 0 {
-      return decryptedCode
-    }
-
-    var curSum: Int = 0
-
-    if k > 0 {
-      curSum = code[1..<(k + 1)].reduce(0, +)
-    } else {
-      curSum = code[codeLen + k..<codeLen].reduce(0, +)
-    }
-
+    var curSum: Int = code[1..<(k + 1)].reduce(0, +)
     decryptedCode[0] = curSum
 
     for left in 1..<codeLen {
-      curSum = calcSum(left, curSum, k)
+      curSum = calcSum(left, curSum, k, code)
       decryptedCode[left] = curSum
     }
 
     return decryptedCode
+  }
 
-    func calcSum(_ left: Int, _ prevSum: Int, _ k: Int) -> Int {
-      var curSum: Int = prevSum
-      var prevLeft: Int = 0
-      var curRight: Int = 0
+  private func negDecrypt(_ code: [Int], _ k: Int) -> [Int] {
+    let codeLen: Int = code.count
+    var decryptedCode: [Int] = Array(repeating: 0, count: codeLen)
 
-      if k > 0 {
-        prevLeft = left
-        curRight = (left + k) % codeLen
-      } else {
-        prevLeft = left + k - 1 >= 0 ? left + k - 1 : (left + k - 1 + codeLen) % codeLen
-        curRight = left - 1 >= 0 ? left - 1 : (left - 1 + codeLen) % codeLen
-      }
+    var curSum: Int = code[codeLen + k..<codeLen].reduce(0, +)
+    decryptedCode[0] = curSum
 
-      curSum -= code[prevLeft]
-      curSum += code[curRight]
-
-      return curSum
+    for left in 1..<codeLen {
+      curSum = calcSum(left, curSum, k, code)
+      decryptedCode[left] = curSum
     }
+
+    return decryptedCode
+  }
+
+  private func calcSum(_ left: Int, _ prevSum: Int, _ k: Int, _ code: [Int]) -> Int {
+    let codeLen: Int = code.count
+    var curSum: Int = prevSum
+    var prevLeft: Int = 0
+    var curRight: Int = 0
+
+    if k > 0 {
+      prevLeft = left
+      curRight = (left + k) % codeLen
+    } else {
+      prevLeft = left + k - 1 >= 0 ? left + k - 1 : (left + k - 1 + codeLen) % codeLen
+      curRight = left - 1 >= 0 ? left - 1 : (left - 1 + codeLen) % codeLen
+    }
+
+    curSum -= code[prevLeft]
+    curSum += code[curRight]
+
+    return curSum
   }
 }
