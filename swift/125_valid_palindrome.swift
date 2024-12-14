3952dd8b6a14eb5bf9389b5f31b8685fc9f3ac88
TakayaShirai <s78taka03@gmail.com>
2024-12-14 15:02:51 -0800

    [swift] Update 125. Valid Palindrome.

--- swift/125_valid_palindrome.swift
+++ swift/125_valid_palindrome.swift
@@ -2,36 +2,40 @@ class Solution {
   func isPalindrome(_ s: String) -> Bool {
     let lowercasedS = s.lowercased()
     let arrayS = Array(lowercasedS)
-    let onlyAlphaNumArrayS = removeNonAlphaChars(arrayS)
+    let onlyNumAlphaS: [Character] = removeNonAlphaNum(arrayS)
 
-    var leftIdx: Int = 0
-    var rightIdx: Int = onlyAlphaNumArrayS.count - 1
+    return checkPalindrome(onlyNumAlphaS)
+  }
 
-    while leftIdx < rightIdx {
-      if onlyAlphaNumArrayS[leftIdx] != onlyAlphaNumArrayS[rightIdx] {
+  private func checkAlphaNum(_ char: Character) -> Bool {
+    return char.isNumber || char.isLetter
+  }
+
+  private func removeNonAlphaNum(_ arrayS: [Character]) -> [Character] {
+    var onlyAlphaNumChars: [Character] = []
+
+    for char in arrayS {
+      if checkAlphaNum(char) {
+        onlyAlphaNumChars.append(char)
+      }
+    }
+
+    return onlyAlphaNumChars
+  }
+
+  private func checkPalindrome(_ arrayS: [Character]) -> Bool {
+    var left: Int = 0
+    var right: Int = arrayS.count - 1
+
+    while left < right {
+      if arrayS[left] != arrayS[right] {
         return false
       }
 
-      leftIdx += 1
-      rightIdx -= 1
+      left += 1
+      right -= 1
     }
 
     return true
-  }
-
-  private func removeNonAlphaChars(_ charArray: [Character]) -> [Character] {
-    var convertedArray: [Character] = []
-
-    for char in charArray {
-      if isAlNum(char) {
-        convertedArray.append(char)
-      }
-    }
-
-    return convertedArray
-  }
-
-  private func isAlNum(_ char: Character) -> Bool {
-    return char.isLetter || char.isNumber
   }
 }
