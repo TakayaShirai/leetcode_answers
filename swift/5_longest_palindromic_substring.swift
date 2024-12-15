97374b388e9c9727b9d5be0eabf39c960d12acf9
TakayaShirai <s78taka03@gmail.com>
2024-12-14 19:32:35 -0800

    [swift] Update 5. Longest Palindromic Substring.

--- swift/5_longest_palindromic_substring.swift
+++ swift/5_longest_palindromic_substring.swift
@@ -34,3 +34,66 @@ class Solution {
     return dpMap
   }
 }
+
+class Solution {
+  func longestPalindrome(_ s: String) -> String {
+    let characters = Array(s)
+    var resultStart = 0
+    var resultEnd = 0
+
+    for center in 0..<characters.count {
+      let oddIndices = findLongestPalindrome(center, center, characters)
+      let evenIndices = findLongestPalindrome(center, center + 1, characters)
+
+      (resultStart, resultEnd) = updateIndices(oddIndices, evenIndices, resultStart, resultEnd)
+    }
+
+    return String(characters[resultStart...resultEnd])
+  }
+
+  private func findLongestPalindrome(_ left: Int, _ right: Int, _ arrayS: [Character]) -> (Int, Int)
+  {
+    var curLeft: Int = left
+    var curRight: Int = right
+    var resLeft: Int = left
+    var resRight: Int = left
+
+    while curLeft >= 0 && curRight < arrayS.count {
+      let leftChar = arrayS[curLeft]
+      let rightChar = arrayS[curRight]
+
+      if leftChar == rightChar {
+        resLeft = curLeft
+        resRight = curRight
+        curLeft -= 1
+        curRight += 1
+      } else {
+        return (resLeft, resRight)
+      }
+    }
+
+    return (resLeft, resRight)
+  }
+
+  private func updateIndices(
+    _ oddIndices: (Int, Int),
+    _ evenIndices: (Int, Int),
+    _ currentStart: Int,
+    _ currentEnd: Int
+  ) -> (Int, Int) {
+    let (oddStart, oddEnd) = oddIndices
+    let (evenStart, evenEnd) = evenIndices
+
+    let oddLength = oddEnd - oddStart + 1
+    let evenLength = evenEnd - evenStart + 1
+    let currentLength = currentEnd - currentStart + 1
+
+    if oddLength > currentLength && oddLength >= evenLength {
+      return (oddStart, oddEnd)
+    } else if evenLength > currentLength {
+      return (evenStart, evenEnd)
+    }
+
+    return (currentStart, currentEnd)
+  }
+}
