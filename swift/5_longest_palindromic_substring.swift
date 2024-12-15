a7359e6ca124a7acfe717ef5dd31c7b5300603e7
TakayaShirai <s78taka03@gmail.com>
2024-12-14 17:36:33 -0800

    [swift] Update 5. Longest Palindromic Substring.

--- swift/5_longest_palindromic_substring.swift
+++ swift/5_longest_palindromic_substring.swift
@@ -1,24 +1,36 @@
 class Solution {
   func longestPalindrome(_ s: String) -> String {
-    let length: Int = s.count
+    guard s.count > 1 else { return s }
+
     let arrayS = Array(s)
-    guard length > 1 else { return s }
+    var dpMap = setUpDpMap(arrayS)
+    var resStart: Int = 0
+    var resEnd: Int = 0
 
-    var left = 0
-    var right = 0
-    var dp = Array(repeating: Array(repeating: 0, count: length), count: length)
+    for substrLen in 2..<(arrayS.count + 1) {
+      for start in 0..<(arrayS.count - substrLen + 1) {
+        let end = start + substrLen - 1
+        if arrayS[start] == arrayS[end] && (substrLen == 2 || dpMap[start + 1][end - 1] == 1) {
+          dpMap[start][end] = 1
 
-    for i in 1..<length {
-      for j in 0..<i {
-        if arrayS[j] == arrayS[i] && (dp[j + 1][i - 1] == 1 || i - j <= 2) {
-          dp[j][i] = 1
-          if i - j > right - left {
-            left = j
-            right = i
+          if substrLen > resEnd - resStart + 1 {
+            resStart = start
+            resEnd = end
           }
         }
       }
     }
-    return String(arrayS[left...right])
+
+    return String(arrayS[resStart..<(resEnd + 1)])
+  }
+
+  private func setUpDpMap(_ arrayS: [Character]) -> [[Int]] {
+    var dpMap = Array(repeating: Array(repeating: 0, count: arrayS.count), count: arrayS.count)
+
+    for i in 0..<arrayS.count {
+      dpMap[i][i] = 1
+    }
+
+    return dpMap
   }
 }
