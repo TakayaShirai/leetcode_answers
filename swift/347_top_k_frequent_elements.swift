48ef358f96dfb3ff83097e290c4fc89c701f6280
TakayaShirai <s78taka03@gmail.com>
2024-12-15 23:52:14 -0800

    [swift] Update 347. Top K Frequent Elements.

--- swift/347_top_k_frequent_elements.swift
+++ swift/347_top_k_frequent_elements.swift
@@ -1,30 +1,41 @@
 class Solution {
   func topKFrequent(_ nums: [Int], _ k: Int) -> [Int] {
-    var counts: [Int: Int] = [:]
-    var buckets: [[Int]] = []
-    var topKElements: [Int] = []
+    let numCntMap: [Int: Int] = createNumCntMap(nums)
+    let numCntBuckets: [[Int]] = createNumCntBuckets(nums.count, numCntMap)
 
-    for _ in 0...nums.count {
-      buckets.append([])
-    }
+    var topKFreqNums: [Int] = []
 
-    for num in nums {
-      counts[num, default: 0] += 1
-    }
-
-    for (key, value) in counts {
-      buckets[value].append(key)
-    }
-
-    for i in (0...nums.count).reversed() {
-      for num in buckets[i] {
-        topKElements.append(num)
-        if topKElements.count == k {
-          return topKElements
+    for bucket in numCntBuckets {
+      if !bucket.isEmpty {
+        for num in bucket {
+          topKFreqNums.append(num)
+          if topKFreqNums.count == k {
+            return topKFreqNums
+          }
         }
       }
     }
 
-    return [-1, -1]
+    return topKFreqNums
+  }
+
+  private func createNumCntMap(_ nums: [Int]) -> [Int: Int] {
+    var numCntMap: [Int: Int] = [:]
+
+    for num in nums {
+      numCntMap[num, default: 0] += 1
+    }
+
+    return numCntMap
+  }
+
+  private func createNumCntBuckets(_ numsCount: Int, _ numCntMap: [Int: Int]) -> [[Int]] {
+    var buckets: [[Int]] = Array(repeating: [], count: numsCount + 1)
+
+    for (key, value) in numCntMap {
+      buckets[value].append(key)
+    }
+
+    return buckets.reversed()
   }
 }
