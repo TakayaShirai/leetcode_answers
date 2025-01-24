e01988fabc3d8f4e061c1c51c53997f6c7ce4430
TakayaShirai <s78taka03@gmail.com>
2025-01-23 23:35:48 -0800

    [swift] Update 463. Island Perimeter.

--- swift/463_island_perimeter.swift
+++ swift/463_island_perimeter.swift
@@ -1,18 +1,16 @@
 class Solution {
   func islandPerimeter(_ grid: [[Int]]) -> Int {
     // BFS
-    let firstLand: (Int, Int) = findLand(grid)
-    if firstLand == (-1, -1) {
-      return 0
-    }
+    let startingLandCell: (Int, Int) = findFirstLandCell(in: grid)
+    guard startingLandCell != (-1, -1) else { return 0 }
 
-    var queue: [(Int, Int)] = []
-    var seenLands: [[Bool]] = Array(
+    var unvisitedLandQueue: [(Int, Int)] = []
+    var visitedCells: [[Bool]] = Array(
       repeating: Array(repeating: false, count: grid[0].count), count: grid.count)
-    var perimeter: Int = 0
+    var totalPerimeter: Int = 0
 
-    queue.append(firstLand)
-    seenLands[firstLand.0][firstLand.1] = true
+    unvisitedLandQueue.append(startingLandCell)
+    visitedCells[startingLandCell.0][startingLandCell.1] = true
 
     let directions: [(Int, Int)] = [
       (-1, 0),
@@ -21,73 +19,44 @@ class Solution {
       (0, -1),
     ]
 
-    while !queue.isEmpty {
-      let curLand: (Int, Int) = queue.removeFirst()
-      var curPerimeter: Int = 4
+    while !unvisitedLandQueue.isEmpty {
+      let curLandCell: (Int, Int) = unvisitedLandQueue.removeFirst()
+      var curCellPerimeter: Int = 4
 
       for direction in directions {
-        let newLandRow: Int = curLand.0 + direction.0
-        let newLandCol: Int = curLand.1 + direction.1
+        let newLandCellRow: Int = curLandCell.0 + direction.0
+        let newLandCellCol: Int = curLandCell.1 + direction.1
 
-        if isLand(newLandRow, newLandCol, grid) {
-          curPerimeter -= 1
+        if isLand(row: newLandCellRow, col: newLandCellCol, in: grid) {
+          curCellPerimeter -= 1
 
-          if !seenLands[newLandRow][newLandCol] {
-            seenLands[newLandRow][newLandCol] = true
-            queue.append((newLandRow, newLandCol))
+          if !visitedCells[newLandCellRow][newLandCellCol] {
+            visitedCells[newLandCellRow][newLandCellCol] = true
+            unvisitedLandQueue.append((newLandCellRow, newLandCellCol))
           }
         }
       }
 
-      perimeter += curPerimeter
+      totalPerimeter += curCellPerimeter
     }
 
-    return perimeter
+    return totalPerimeter
 
     // Double For Loop
-    // var perimeter: Int = 0
+    // var totalPerimeter: Int = 0
 
     // for row in 0..<grid.count {
     //     for col in 0..<grid[0].count {
     //         if grid[row][col] == 1 {
-    //             perimeter += countSurroundingsWater(row, col, grid)
+    //             totalPerimeter += countSurroundingsWater(row: row, col: col, in: grid)
     //         }
     //     }
     // }
 
-    // return perimeter
+    // return totalPerimeter
   }
 
-  private func countSurroundingsWater(_ row: Int, _ col: Int, _ grid: [[Int]]) -> Int {
-    let directions: [(Int, Int)] = [
-      (-1, 0),
-      (1, 0),
-      (0, 1),
-      (0, -1),
-    ]
-    var count: Int = 0
-
-    for direction in directions {
-      let newRow: Int = row + direction.0
-      let newCol: Int = col + direction.1
-
-      if isWater(newRow, newCol, grid) {
-        count += 1
-      }
-    }
-
-    return count
-  }
-
-  private func isWater(_ row: Int, _ col: Int, _ grid: [[Int]]) -> Bool {
-    if row < 0 || row >= grid.count || col < 0 || col >= grid[0].count {
-      return true
-    }
-
-    return grid[row][col] == 0
-  }
-
-  private func findLand(_ grid: [[Int]]) -> (Int, Int) {
+  private func findFirstLandCell(in grid: [[Int]]) -> (Int, Int) {
     for row in 0..<grid.count {
       for col in 0..<grid[0].count {
         if grid[row][col] == 1 {
@@ -99,8 +68,34 @@ class Solution {
     return (-1, -1)
   }
 
-  private func isLand(_ row: Int, _ col: Int, _ grid: [[Int]]) -> Bool {
+  private func isLand(row: Int, col: Int, in grid: [[Int]]) -> Bool {
     guard 0 <= row && row < grid.count && 0 <= col && col < grid[0].count else { return false }
     return grid[row][col] == 1
   }
+
+  // private func countSurroundingsWater(row: Int, col: Int, in grid: [[Int]]) -> Int {
+  //     let directions: [(Int, Int)] = [
+  //         (-1, 0),
+  //         (1, 0),
+  //         (0, 1),
+  //         (0, -1)
+  //     ]
+  //     var waterCount: Int = 0
+
+  //     for direction in directions {
+  //         let newRow: Int = row + direction.0
+  //         let newCol: Int = col + direction.1
+
+  //         if isWater(row: newRow, col: newCol, in: grid) {
+  //             waterCount += 1
+  //         }
+  //     }
+
+  //     return waterCount
+  // }
+
+  // private func isWater(row: Int, col: Int, in grid: [[Int]]) -> Bool {
+  //     guard row >= 0 && row < grid.count && col >= 0 && col < grid[0].count else { return true }
+  //     return grid[row][col] == 0
+  // }
 }
