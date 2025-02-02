86bfee602b55b7f46a684dcbbebf5482f623959b
TakayaShirai <s78taka03@gmail.com>
2025-02-01 18:48:23 -0800

    [swift] Update 153. Find Minimum in Rotated Sorted Array.

--- swift/153_find_minimum_in_rotated_sorted_array.swift
+++ swift/153_find_minimum_in_rotated_sorted_array.swift
@@ -2,10 +2,9 @@ class Solution {
   func findMin(_ nums: [Int]) -> Int {
     var left: Int = 0
     var right: Int = nums.count - 1
-    var mid: Int = 0
 
     while left < right {
-      mid = (left + right) / 2
+      let mid: Int = left + (right - left) / 2
 
       if nums[mid] > nums[right] {
         left = mid + 1
