ccb69790a93a6df4f4c79d7e4b2e547e985c6723
TakayaShirai <s78taka03@gmail.com>
2025-02-01 18:24:07 -0800

    [swift] Update 33. Search in Rotated Sorted Array.

--- swift/33_search_in_rotated_sorted_array.swift
+++ swift/33_search_in_rotated_sorted_array.swift
@@ -1,33 +1,43 @@
 class Solution {
   func search(_ nums: [Int], _ target: Int) -> Int {
+    let rotationCnt: Int = findMinValIdx(from: nums)
+    let numsLen: Int = nums.count
+
+    print(rotationCnt)
+
     var left: Int = 0
-    var right: Int = nums.count - 1
-    var mid: Int = 0
+    var right: Int = numsLen - 1
 
     while left <= right {
-      mid = (left + right) / 2
+      let mid: Int = left + (right - left) / 2
+      let shiftedMid: Int = (mid + rotationCnt) % numsLen
 
-      if nums[mid] == target {
-        return mid
-      } else if nums[left] <= nums[mid] {
-        if nums[mid] < target {
-          left = mid + 1
-        } else if nums[left] > target {
-          left = mid + 1
-        } else {
-          right = mid - 1
-        }
+      if nums[shiftedMid] == target {
+        return shiftedMid
+      } else if nums[shiftedMid] < target {
+        left = mid + 1
       } else {
-        if nums[mid] > target {
-          right = mid - 1
-        } else if nums[right] < target {
-          right = mid - 1
-        } else {
-          left = mid + 1
-        }
+        right = mid - 1
       }
     }
 
     return -1
+  }
+
+  private func findMinValIdx(from nums: [Int]) -> Int {
+    var left: Int = 0
+    var right: Int = nums.count - 1
+
+    while left < right {
+      let mid: Int = left + (right - left) / 2
+
+      if nums[mid] > nums[right] {
+        left = mid + 1
+      } else {
+        right = mid
+      }
+    }
+
+    return left
   }
 }
