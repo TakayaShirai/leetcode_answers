0e6dae7caf7c14b035e1ffe243ebe1bf23c77175
TakayaShirai <s78taka03@gmail.com>
2025-02-11 23:11:00 -0800

    [swift] Update 748. Shortest Completing Word.

--- swift/748_shortest_completing_word.swift
+++ swift/748_shortest_completing_word.swift
@@ -1,44 +1,99 @@
+import XCTest
+
 class Solution {
+  /// Returns the shortest completing word for the given license plate from the provided list of words.
+  ///
+  /// - Parameters:
+  ///   - licensePlate: The string to be searched for a completing word.
+  ///   - words: The collection of the words to search for a completing word.
+  /// - Returns: The shortest completing word of the license plate in words.
   func shortestCompletingWord(_ licensePlate: String, _ words: [String]) -> String {
-    let lpCharCntMap: [Character: Int] = createCharCntMap(of: licensePlate)
     var shortestCompWord: String? = nil
 
-    for word in words {
-      if isCompletingWord(word, of: lpCharCntMap) {
-        if shortestCompWord == nil || word.count < shortestCompWord!.count {
-          shortestCompWord = word
-        }
+    for word in words where isCompletingWord(word, of: licensePlate) {
+      if shortestCompWord == nil || word.count < shortestCompWord!.count {
+        shortestCompWord = word
       }
     }
 
-    return shortestCompWord!
+    return shortestCompWord ?? ""
   }
 
-  private func createCharCntMap(of str: String) -> [Character: Int] {
+  /// Returns a boolean value indicating whether the given word is a completing word of the given string.
+  ///
+  /// - Parameters:
+  ///   - word: The word to be checked.
+  ///   - target: The reference string containing required characters.
+  /// - Returns: The boolean value indicating whether the word is a completing word of the given string.
+  private func isCompletingWord(_ word: String, of target: String) -> Bool {
+    let wordCharCntMap: [Character: Int] = createAlphaCharCntMap(of: word.lowercased())
+    let targetCharCntMap: [Character: Int] = createAlphaCharCntMap(of: target.lowercased())
+
+    for (char, requiredCnt) in targetCharCntMap {
+      guard let availableCnt = wordCharCntMap[char] else { return false }
+      guard availableCnt >= requiredCnt else { return false }
+    }
+
+    return true
+  }
+
+  /// Returns a character count hashmap for alphabetical characters in the given string.
+  ///
+  /// - Parameters:
+  ///   - str: The string from which the hashmap is created.
+  /// - Returns: The character count hashmap for alphabetical characters.
+  private func createAlphaCharCntMap(of str: String) -> [Character: Int] {
     var charCntMap: [Character: Int] = [:]
 
-    for char in str.lowercased() where char.isLetter {
+    for char in str where char != " " && char.isLetter {
       charCntMap[char, default: 0] += 1
     }
 
     return charCntMap
   }
+}
 
-  private func isCompletingWord(_ word: String, of charCntMap: [Character: Int]) -> Bool {
-    var curCharMap = charCntMap
+class SolutionTest: XCTestCase {
+  var solution: Solution!
 
-    for char in word.lowercased() where char.isLetter {
-      if curCharMap[char] != nil {
-        curCharMap[char]! -= 1
-      }
-    }
+  override func setUp() {
+    super.setUp()
+    solution = Solution()
+  }
 
-    for value in curCharMap.values {
-      if value > 0 {
-        return false
-      }
-    }
+  func testShortestCompletingWordWhenGivenAlphaOnlyLicensePlate() {
+    let licensePlate = "abcd"
+    let words = ["afv", "ahirg", "abbcccdd", "bbacda"]
+    let shortestCompWord = solution.shortestCompletingWord(licensePlate, words)
+    XCTAssertEqual(
+      shortestCompWord, "bbacda",
+      "Should return 'bbacda' but it returned \(shortestCompWord) instead.")
+  }
 
-    return true
+  func testShortestCompletingWordWhenGivenLicensePlateContainsSpace() {
+    let licensePlate = "a b c d"
+    let words = ["afv", "ahirg", "abbcccdd", "bbacda"]
+    let shortestCompWord = solution.shortestCompletingWord(licensePlate, words)
+    XCTAssertEqual(
+      shortestCompWord, "bbacda",
+      "Should return 'bbacda' but it returned \(shortestCompWord) instead.")
+  }
+
+  func testShortestCompletingWordWhenGivenLicensePlateContainsNumber() {
+    let licensePlate = "abc1d2"
+    let words = ["afv", "ahirg", "abbcccdd", "bbacda"]
+    let shortestCompWord = solution.shortestCompletingWord(licensePlate, words)
+    XCTAssertEqual(
+      shortestCompWord, "bbacda",
+      "Should return 'bbacda' but it returned \(shortestCompWord) instead.")
+  }
+
+  func testShortestCompletingWordWhenGivenLicensePlateContainsSpaceAndNums() {
+    let licensePlate = "a1b c d"
+    let words = ["afv", "ahirg", "abbcccdd", "bbacda"]
+    let shortestCompWord = solution.shortestCompletingWord(licensePlate, words)
+    XCTAssertEqual(
+      shortestCompWord, "bbacda",
+      "Should return 'bbacda' but it returned \(shortestCompWord) instead.")
   }
 }
