415e504d8310b1e9c74bbe47d86fdbf6a40af810
TakayaShirai <s78taka03@gmail.com>
2025-02-18 16:03:55 -0800

    [swift] Update 104. Maximum Depth of Binary Tree.

--- swift/104_maximum_depth_of_binary_tree.swift
+++ swift/104_maximum_depth_of_binary_tree.swift
@@ -13,68 +13,7 @@
 /// }
 class Solution {
   func maxDepth(_ root: TreeNode?) -> Int {
-    return iterativeDfsMaxDepth(root)
-    // return recursiveDfsMaxDepth(root)
-    // return bfsMaxDepth(root)
-  }
-
-  func recursiveDfsMaxDepth(_ root: TreeNode?) -> Int {
-    if root == nil {
-      return 0
-    }
-
-    return 1 + max(recursiveDfsMaxDepth(root!.left), recursiveDfsMaxDepth(root!.right))
-  }
-
-  func iterativeDfsMaxDepth(_ root: TreeNode?) -> Int {
-    if root == nil {
-      return 0
-    }
-
-    var maxDepth: Int = 1
-    var stack: [(node: TreeNode, depth: Int)] = [(root!, 1)]
-
-    while !stack.isEmpty {
-      let poppedItem = stack.popLast()!
-      var curNode = poppedItem.node
-      var curDepth = poppedItem.depth
-
-      maxDepth = max(maxDepth, curDepth)
-
-      if curNode.right != nil {
-        stack.append((curNode.right!, curDepth + 1))
-      }
-
-      if curNode.left != nil {
-        stack.append((curNode.left!, curDepth + 1))
-      }
-    }
-
-    return maxDepth
-  }
-
-  func bfsMaxDepth(_ root: TreeNode?) -> Int {
-    guard var cur = root else { return 0 }
-
-    var depth: Int = 0
-    var queue: [TreeNode] = [cur]
-
-    while !queue.isEmpty {
-      var tmpCount = queue.count
-      depth += 1
-
-      for _ in 0..<tmpCount {
-        cur = queue.removeFirst()
-        if cur.left != nil {
-          queue.append(cur.left!)
-        }
-
-        if cur.right != nil {
-          queue.append(cur.right!)
-        }
-      }
-    }
-
-    return depth
+    guard let root = root else { return 0 }
+    return max(maxDepth(root.left), maxDepth(root.right)) + 1
   }
 }
