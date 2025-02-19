b700b40ff578b47ae25ff093f5185e6c3951c36d
TakayaShirai <s78taka03@gmail.com>
2025-02-18 16:19:01 -0800

    [swift] Update 110. Balanced Binary Tree.

--- swift/110_balanced_binary_tree.swift
+++ swift/110_balanced_binary_tree.swift
@@ -13,53 +13,20 @@
 /// }
 class Solution {
   func isBalanced(_ root: TreeNode?) -> Bool {
-    return calcHeightAndIsBalanced(root).isBalanced
-
-    func calcHeightAndIsBalanced(_ root: TreeNode?) -> (isBalanced: Bool, height: Int) {
-      if root == nil {
-        return (true, 0)
-      }
-
-      let left = calcHeightAndIsBalanced(root!.left)
-      let right = calcHeightAndIsBalanced(root!.right)
-
-      let isBalanced =
-        (left.isBalanced && right.isBalanced) && (abs(left.height - right.height) <= 1)
-
-      return (isBalanced, (max(left.height, right.height) + 1))
-    }
+    return isBalancedWithHeight(root).isBalanced
   }
-}
 
-/// Definition for a binary tree node.
-/// public class TreeNode {
-///     public var val: Int
-///     public var left: TreeNode?
-///     public var right: TreeNode?
-///     public init() { self.val = 0; self.left = nil; self.right = nil; }
-///     public init(_ val: Int) { self.val = val; self.left = nil; self.right = nil; }
-///     public init(_ val: Int, _ left: TreeNode?, _ right: TreeNode?) {
-///         self.val = val
-///         self.left = left
-///         self.right = right
-///     }
-/// }
-class Solution {
-  func isBalanced(_ root: TreeNode?) -> Bool {
-    return calcHeightAndIsBalanced(root).isBalanced
+  private func isBalancedWithHeight(_ root: TreeNode?) -> (isBalanced: Bool, height: Int) {
+    guard let root = root else { return (true, 0) }
 
-    func calcHeightAndIsBalanced(_ root: TreeNode?) -> (isBalanced: Bool, height: Int) {
-      if root == nil {
-        return (true, 0)
-      }
+    let leftSubtree = isBalancedWithHeight(root.left)
+    let rightSubtree = isBalancedWithHeight(root.right)
 
-      let left = calcHeightAndIsBalanced(root!.left)
-      let right = calcHeightAndIsBalanced(root!.right)
+    let isBalanced: Bool =
+      (leftSubtree.isBalanced && rightSubtree.isBalanced)
+      && abs(leftSubtree.height - rightSubtree.height) <= 1
+    let height: Int = max(leftSubtree.height, rightSubtree.height) + 1
 
-      let isBalanced =
-        (left.isBalanced && right.isBalanced) && (abs(left.height - right.height) <= 1)
-
-      return (isBalanced, (max(left.height, right.height) + 1))
-    }
+    return (isBalanced, height)
   }
 }
