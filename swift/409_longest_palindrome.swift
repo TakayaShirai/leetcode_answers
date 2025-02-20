04bef922ad2efcb5b9130a89d2ef115c064ebcbe
TakayaShirai <s78taka03@gmail.com>
2025-02-20 14:02:46 -0800

    [swift] Update 409. Longest Palindrome.

--- swift/409_longest_palindrome.swift
+++ swift/409_longest_palindrome.swift
@@ -1,67 +1,32 @@
 class Solution {
   func longestPalindrome(_ s: String) -> Int {
-    // Hash Set
-    var seenChars: [Character: Int] = [:]
-    var maxLen: Int = 0
+    var charCounter: [Character: Int] = createCharCounter(of: s)
+    var existsOddCntChar: Bool = false
+    var longestPalindromeCnt: Int = 0
 
-    for char in s {
-      seenChars[char, default: 0] += 1
-    }
-
-    var existOddCnt: Bool = false
-
-    for seenCharCnt in seenChars.values {
-      if seenCharCnt % 2 == 0 {
-        maxLen += seenCharCnt
+    for count in charCounter.values {
+      if count % 2 == 0 {
+        longestPalindromeCnt += count
       } else {
-        existOddCnt = true
-        maxLen += seenCharCnt - 1
+        if existsOddCntChar {
+          longestPalindromeCnt += count - 1
+        } else {
+          longestPalindromeCnt += count
+          existsOddCntChar = true
+        }
       }
     }
 
-    if existOddCnt {
-      maxLen += 1
+    return longestPalindromeCnt
+  }
+
+  private func createCharCounter(of str: String) -> [Character: Int] {
+    var charCounter: [Character: Int] = [:]
+
+    for char in str {
+      charCounter[char, default: 0] += 1
     }
 
-    return maxLen
-
-    // Greedy
-    // var seenChars: [Character : Int] = [:]
-    // var oddCharCnt: Int = 0
-
-    // for char in s {
-    //     seenChars[char, default: 0] += 1
-
-    //     if seenChars[char]! % 2 == 1 {
-    //         oddCharCnt += 1
-    //     } else {
-    //         oddCharCnt -= 1
-    //     }
-    // }
-
-    // if oddCharCnt > 0 {
-    //     return s.count - oddCharCnt + 1
-    // } else {
-    //     return s.count
-    // }
-
-    // Greedy with hash set
-    // var seenChars: Set<Character> = []
-    // var maxLen: Int = 0
-
-    // for char in s {
-    //     if seenChars.contains(char) {
-    //         seenChars.remove(char)
-    //         maxLen += 2
-    //     } else {
-    //         seenChars.insert(char)
-    //     }
-    // }
-
-    // if seenChars.count > 0 {
-    //      maxLen += 1
-    // }
-
-    // return maxLen
+    return charCounter
   }
 }
