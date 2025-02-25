fe4649e0ea2377e59f54d0d6eb36c3663ce7353f
TakayaShirai <s78taka03@gmail.com>
2025-02-25 12:47:47 -0800

    [swift] Update 859. Buddy Strings.

--- swift/859_buddy_strings.swift
+++ swift/859_buddy_strings.swift
@@ -1,4 +1,13 @@
+import XCTest
+
 class Solution {
+  /// Return a boolean value indicating whether the two given strings can become identical
+  /// by swapping two charactes.
+  ///
+  /// - Parameters:
+  ///   - s: The first input string.
+  ///   - goal: The string to compare with the given string s.
+  /// - Returns: The boolean value indicating whether they can be identical by swapping two characters.
   func buddyStrings(_ s: String, _ goal: String) -> Bool {
     guard s.count == goal.count else { return false }
 
@@ -10,17 +19,59 @@ class Solution {
     }
   }
 
-  private func canSwapExactlyTwoChars(_ s: String, _ goal: String) -> Bool {
+  /// Check if the given two strings can become identical by swapping exactly two different characters.
+  ///
+  /// - Parameters:
+  ///   - str1: The first input string.
+  ///   - str2: The second input string.
+  /// - Returns: The boolean value indicating if the given strings can become identical.
+  private func canSwapExactlyTwoChars(_ str1: String, _ str2: String) -> Bool {
     let requiredSwapCnt: Int = 2
     var diffChars: [[Character]] = []
 
-    for (sChar, goalChar) in zip(s, goal) {
-      if sChar != goalChar {
-        diffChars.append([sChar, goalChar])
-        guard diffChars.count <= requiredSwapCnt else { return false }
-      }
+    for (str1Char, str2Char) in zip(str1, str2) where str1Char != str2Char {
+      diffChars.append([str1Char, str2Char])
+      guard diffChars.count <= requiredSwapCnt else { return false }
     }
 
+    // Check if exactly two characters differ and if swapping them makes the strings identical.
     return diffChars.count == requiredSwapCnt && diffChars[0] == diffChars[1].reversed()
+  }
+}
+
+class SolutionTest: XCTestCase {
+  var solution: Solution!
+
+  override func setUp() {
+    super.setUp()
+    solution = Solution()
+  }
+
+  func testBuddyStringsWhenGivenIdenticalStringsWithDuplicates() {
+    let s: String = "abcdea"
+    let goal: String = "abcdea"
+    let isBuddyStrings: Bool = solution.buddyStrings(s, goal)
+    XCTAssertEqual(isBuddyStrings, true, "Should return 'true' but returned 'false'.")
+  }
+
+  func testBuddyStringsWhenGivenIdenticalStringsWithoutDuplicates() {
+    let s: String = "abcde"
+    let goal: String = "abcde"
+    let isBuddyStrings: Bool = solution.buddyStrings(s, goal)
+    XCTAssertEqual(isBuddyStrings, false, "Should return 'false' but returned 'true'.")
+  }
+
+  func testBuddyStringsWhenGivenTwoCharactersDifferingStrings() {
+    let s: String = "abcde"
+    let goal: String = "adcbe"
+    let isBuddyStrings: Bool = solution.buddyStrings(s, goal)
+    XCTAssertEqual(isBuddyStrings, true, "Should return 'true' but returned 'false'.")
+  }
+
+  func testBuddyStringsWhenGivenMoreThanTwoCharactersDifferingStrings() {
+    let s: String = "abcde"
+    let goal: String = "abecd"
+    let isBuddyStrings: Bool = solution.buddyStrings(s, goal)
+    XCTAssertEqual(isBuddyStrings, false, "Should return 'false' but returned 'true'.")
   }
 }
