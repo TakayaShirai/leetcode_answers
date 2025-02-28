a89f21461aa13d6df209d229facaad1253d63748
TakayaShirai <s78taka03@gmail.com>
2025-02-27 18:22:44 -0800

    [swift] Update 200. Number of Islands.

--- swift/200_number_of_islands.swift
+++ swift/200_number_of_islands.swift
@@ -1,70 +1,66 @@
+struct DirectionIterator: IteratorProtocol {
+  private let directions: [(row: Int, col: Int)] = [
+    (row: 1, col: 0),
+    (row: -1, col: 0),
+    (row: 0, col: -1),
+    (row: 0, col: 1),
+  ]
+  private var curIdx: Int = 0
+
+  mutating func next() -> (row: Int, col: Int)? {
+    guard curIdx < directions.count else { return nil }
+    let direction = directions[curIdx]
+    curIdx += 1
+    return direction
+  }
+}
+
 class Solution {
   func numIslands(_ grid: [[Character]]) -> Int {
-    let rowLen: Int = grid.count
-    let columnLen: Int = grid[0].count
+    var seenLands: [[Bool]] = Array(
+      repeating: Array(repeating: false, count: grid[0].count), count: grid.count)
+    var islandCnt: Int = 0
 
-    var seenGrids = Array(repeating: Array(repeating: 0, count: columnLen), count: rowLen)
-    var countIslands: Int = 0
+    for row in 0..<grid.count {
+      for col in 0..<grid[0].count {
+        if grid[row][col] == "1" && !seenLands[row][col] {
+          print(row, col)
+          findLands(from: (row, col), grid: grid, seenLands: &seenLands)
+          islandCnt += 1
+        }
+      }
+    }
 
-    for row in 0..<rowLen {
-      for column in 0..<columnLen {
-        if seenGrids[row][column] == 1 || grid[row][column] == "0" {
+    return islandCnt
+  }
+
+  private func findLands(
+    from pos: (row: Int, col: Int), grid: [[Character]], seenLands: inout [[Bool]]
+  ) {
+    var landQueue: [(row: Int, col: Int)] = []
+    landQueue.append(pos)
+
+    while !landQueue.isEmpty {
+      let curLandPos = landQueue.removeFirst()
+      let curRow: Int = curLandPos.row
+      let curCol: Int = curLandPos.col
+
+      var directionIterator = DirectionIterator()
+
+      while let direction = directionIterator.next() {
+        let newRow: Int = curRow + direction.row
+        let newCol: Int = curCol + direction.col
+
+        guard 0 <= newRow && newRow < grid.count && 0 <= newCol && newCol < grid[0].count else {
           continue
-        } else {
-          bfs(row, column)
-          countIslands += 1
+        }
+
+        if grid[newRow][newCol] == "1" && !seenLands[curRow][curCol] {
+          landQueue.append((newRow, newCol))
         }
       }
+
+      seenLands[curRow][curCol] = true
     }
-
-    func bfs(_ row: Int, _ column: Int) {
-      guard row < rowLen, column < columnLen else { return }
-
-      var queue: [Int] = []
-
-      queue.append(row)
-      queue.append(column)
-
-      while !queue.isEmpty {
-        let curRow: Int = queue.removeFirst()
-        let curColumn: Int = queue.removeFirst()
-
-        seenGrids[curRow][curColumn] = 1
-
-        if curRow - 1 >= 0 && grid[curRow - 1][curColumn] == "1"
-          && seenGrids[curRow - 1][curColumn] == 0
-        {
-          queue.append(curRow - 1)
-          queue.append(curColumn)
-          seenGrids[curRow - 1][curColumn] = 1
-        }
-
-        if curRow + 1 < rowLen && grid[curRow + 1][curColumn] == "1"
-          && seenGrids[curRow + 1][curColumn] == 0
-        {
-          queue.append(curRow + 1)
-          queue.append(curColumn)
-          seenGrids[curRow + 1][curColumn] = 1
-        }
-
-        if curColumn - 1 >= 0 && grid[curRow][curColumn - 1] == "1"
-          && seenGrids[curRow][curColumn - 1] == 0
-        {
-          queue.append(curRow)
-          queue.append(curColumn - 1)
-          seenGrids[curRow][curColumn - 1] = 1
-        }
-
-        if curColumn + 1 < columnLen && grid[curRow][curColumn + 1] == "1"
-          && seenGrids[curRow][curColumn + 1] == 0
-        {
-          queue.append(curRow)
-          queue.append(curColumn + 1)
-          seenGrids[curRow][curColumn + 1] = 1
-        }
-      }
-    }
-
-    return countIslands
   }
 }
