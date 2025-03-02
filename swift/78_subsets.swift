7078a550b17b2d965c2b41e297b1229fda299124
TakayaShirai <s78taka03@gmail.com>
2025-03-01 16:03:56 -0800

    [swift] Update 78. Subsets.

--- swift/78_subsets.swift
+++ swift/78_subsets.swift
@@ -1,22 +1,21 @@
 class Solution {
   func subsets(_ nums: [Int]) -> [[Int]] {
+    var curSubset: [Int] = []
     var subsets: [[Int]] = []
-
-    dfs(depth: 0, nums, curSubset: [], subsets: &subsets)
+    backtrack(nums: nums, curIdx: 0, curSubset: &curSubset, subsets: &subsets)
     return subsets
   }
 
-  private func dfs(depth: Int, _ nums: [Int], curSubset: [Int], subsets: inout [[Int]]) {
-    if depth == nums.count {
+  private func backtrack(nums: [Int], curIdx: Int, curSubset: inout [Int], subsets: inout [[Int]]) {
+    guard curIdx != nums.count else {
       subsets.append(curSubset)
       return
     }
 
-    var subset = curSubset
-    subset.append(nums[depth])
-    dfs(depth: depth + 1, nums, curSubset: subset, subsets: &subsets)
+    curSubset.append(nums[curIdx])
+    backtrack(nums: nums, curIdx: curIdx + 1, curSubset: &curSubset, subsets: &subsets)
 
-    subset.removeLast()
-    dfs(depth: depth + 1, nums, curSubset: subset, subsets: &subsets)
+    curSubset.removeLast()
+    backtrack(nums: nums, curIdx: curIdx + 1, curSubset: &curSubset, subsets: &subsets)
   }
 }
