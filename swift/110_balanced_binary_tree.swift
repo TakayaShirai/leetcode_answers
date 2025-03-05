661eb21c6169147f1c02dcb2ba6c3f7776bbd059
TakayaShirai <s78taka03@gmail.com>
2025-03-04 23:11:37 -0800

    [swift] Update 110. Balanced Binary Tree.

--- swift/110_balanced_binary_tree.swift
+++ swift/110_balanced_binary_tree.swift
@@ -1,26 +1,27 @@
-/// Definition for a binary tree node.
-/// public class TreeNode {
-///     public var val: Int
-///     public var left: TreeNode?
-///     public var right: TreeNode?
-///     public init() { self.val = 0; self.left = nil; self.right = nil; }
-///     public init(_ val: Int) { self.val = val; self.left = nil; self.right = nil; }
-///     public init(_ val: Int, _ left: TreeNode?, _ right: TreeNode?) {
-///         self.val = val
-///         self.left = left
-///         self.right = right
-///     }
-/// }
+import XCTest
+
 class Solution {
-  func isBalanced(_ root: TreeNode?) -> Bool {
-    return isBalancedWithHeight(root).isBalanced
+  /// Check if the given tree is a balanced binary tree.
+  ///
+  /// - Parameters:
+  ///   - root: The root node of the given tree.
+  /// - Returns:
+  ///   - The boolean value indicating if the given tree is a balanced binary tree.
+  func isBalanced(root: TreeNode?) -> Bool {
+    return isBalancedWithHeight(root: root).isBalanced
   }
 
-  private func isBalancedWithHeight(_ root: TreeNode?) -> (isBalanced: Bool, height: Int) {
+  /// Check if the given tree is a balanced binary tree while calculating its height.
+  ///
+  /// - Parameters:
+  ///   - root: The root node of the given tree.
+  /// - Returns:
+  ///   - The tuple consisting of the boolean value indicating if it is a balanced, and its height.
+  private func isBalancedWithHeight(root: TreeNode?) -> (isBalanced: Bool, height: Int) {
     guard let root = root else { return (true, 0) }
 
-    let leftSubtree = isBalancedWithHeight(root.left)
-    let rightSubtree = isBalancedWithHeight(root.right)
+    let leftSubtree = isBalancedWithHeight(root: root.left)
+    let rightSubtree = isBalancedWithHeight(root: root.right)
 
     let isBalanced: Bool =
       (leftSubtree.isBalanced && rightSubtree.isBalanced)
@@ -28,5 +29,43 @@ class Solution {
     let height: Int = max(leftSubtree.height, rightSubtree.height) + 1
 
     return (isBalanced, height)
+  }
+}
+
+class SolutionTest: XCTestCase {
+  var solution: Solution!
+
+  override func setUp() {
+    super.setUp()
+    solution = Solution()
+  }
+
+  private func convertNumsToTree(nums: [Int?], nodePos: Int) -> TreeNode? {
+    guard nodePos < nums.count, let curNodeVal = nums[nodePos] else { return nil }
+
+    let rootNode = TreeNode(curNodeVal)
+    rootNode.left = convertNumsToTree(nums: nums, nodePos: nodePos * 2 + 1)
+    rootNode.right = convertNumsToTree(nums: nums, nodePos: nodePos * 2 + 2)
+
+    return rootNode
+  }
+
+  func testIsBalancedWhenGivenNil() {
+    let isBalanced: Bool = solution.isBalanced(root: nil)
+    XCTAssertEqual(isBalanced, true, "Should return 'true' but returned \(isBalanced).")
+  }
+
+  func testIsBalancedWhenGivenBalancedTree() {
+    let nums: [Int?] = [1, 2, 3, 4, 5, 6, 7]
+    let root: TreeNode? = convertNumsToTree(nums: nums, nodePos: 0)
+    let isBalanced: Bool = solution.isBalanced(root: root)
+    XCTAssertEqual(isBalanced, true, "Should return 'true' but returned \(isBalanced).")
+  }
+
+  func testIsBalancedWhenGivenNonBalancedTree() {
+    let nums: [Int?] = [1, 2, nil, 3, nil, 4]
+    let root: TreeNode? = convertNumsToTree(nums: nums, nodePos: 0)
+    let isBalanced: Bool = solution.isBalanced(root: root)
+    XCTAssertEqual(isBalanced, false, "Should return 'false' but returned \(isBalanced).")
   }
 }
