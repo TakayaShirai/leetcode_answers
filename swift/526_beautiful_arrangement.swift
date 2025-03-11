e12bcb513fd9410250327f56d5367621c29c46cb
TakayaShirai <s78taka03@gmail.com>
2025-03-11 15:04:13 -0700

    [swift] Update 526. Beautiful Arrangement.

--- swift/526_beautiful_arrangement.swift
+++ swift/526_beautiful_arrangement.swift
@@ -1,28 +1,70 @@
+import XCTest
+
 class Solution {
-  func countArrangement(_ n: Int) -> Int {
+  /// Returns the number of beautiful arrangements that can be formed using numbers 1 through the given maximum.
+  ///
+  /// - Parameter: maxNum: The maximum number used to create beautiful arrangements.
+  /// - Returns: The number of beautiful arrangements that can be formed using numbers 1 through the given maximum.
+  func countBeautifulArrangements(maxNum: Int) -> Int {
     var beautifulArrCnt = 0
     var usedNums = Set<Int>()
-    searchValidPermutations(
-      maxVal: n, curIdx: 1, usedNums: &usedNums, beautifulArrCnt: &beautifulArrCnt)
+    findBeautifulArrangements(
+      maxNum: maxNum, curIdx: 1, usedNums: &usedNums, beautifulArrCnt: &beautifulArrCnt)
     return beautifulArrCnt
   }
 
-  private func searchValidPermutations(
-    maxVal: Int, curIdx: Int, usedNums: inout Set<Int>, beautifulArrCnt: inout Int
+  /// Finds beautiful arrangements recursively using backtracking and by checking all possible arrangements.
+  ///
+  /// - Parameters:
+  ///   - maxNum: The maximum number used to create an arrangement.
+  ///   - curIdx: The current index in the arrangement.
+  ///   - usedNums: The set of the numbers already used in the arrangement.
+  ///   - beautifulArrCnt: The current count of found beautiful arrangements.
+  private func findBeautifulArrangements(
+    maxNum: Int, curIdx: Int, usedNums: inout Set<Int>, beautifulArrCnt: inout Int
   ) {
-    if curIdx > maxVal {
+    if curIdx > maxNum {
       beautifulArrCnt += 1
       return
     }
 
-    for num in 1...maxVal {
+    for num in 1...maxNum {
       guard !usedNums.contains(num) else { continue }
       guard num % curIdx == 0 || curIdx % num == 0 else { continue }
 
       usedNums.insert(num)
-      searchValidPermutations(
-        maxVal: maxVal, curIdx: curIdx + 1, usedNums: &usedNums, beautifulArrCnt: &beautifulArrCnt)
+      findBeautifulArrangements(
+        maxNum: maxNum, curIdx: curIdx + 1, usedNums: &usedNums, beautifulArrCnt: &beautifulArrCnt)
       usedNums.remove(num)
     }
+  }
+}
+
+class SolutionTest: XCTestCase {
+  var solution: Solution!
+
+  override func setUp() {
+    super.setUp()
+    solution = Solution()
+  }
+
+  func testCountBeautifulArrangementsWhenGivenSmallestValue() {
+    let inputMaxNum: Int = 1
+    let beautifulArrangementCnt: Int = solution.countBeautifulArrangements(maxNum: inputMaxNum)
+    let expectedCnt: Int = 1
+    XCTAssertEqual(
+      beautifulArrangementCnt, expectedCnt,
+      "Should return \(expectedCnt), but returned \(beautifulArrangementCnt)."
+    )
+  }
+
+  func testCountBeautifulArrangementsWhenGivenLargestValue() {
+    let inputMaxNum: Int = 15
+    let beautifulArrangementCnt: Int = solution.countBeautifulArrangements(maxNum: inputMaxNum)
+    let expectedCnt: Int = 24679
+    XCTAssertEqual(
+      beautifulArrangementCnt, expectedCnt,
+      "Should return \(expectedCnt), but returned \(beautifulArrangementCnt)."
+    )
   }
 }
