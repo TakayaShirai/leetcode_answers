2223dc514731cc8544d5e1daacc74b4e32390f75
TakayaShirai <s78taka03@gmail.com>
2025-04-10 23:45:20 -0700

    [swift] Update 27. Remove Element.

--- swift/27_remove_element.swift
+++ swift/27_remove_element.swift
@@ -12,6 +12,26 @@ class Solution {
 
     return left
 
+    // var curIdx: Int = 0
+    // var swapIdx: Int = nums.count - 1
+
+    // while curIdx <= swapIdx {
+    //   let curNum: Int = nums[curIdx]
+
+    //   guard curNum != val else {
+    //     let tmp: Int = nums[swapIdx]
+    //     nums[swapIdx] = curNum
+    //     nums[curIdx] = tmp
+    //     nums.removeLast()
+    //     swapIdx -= 1
+    //     continue
+    //   }
+
+    //   curIdx += 1
+    // }
+
+    // return nums.count
+
     // O(n^2)
     // var curIdx: Int = 0
 
