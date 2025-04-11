5350f34839916fe6dcbf4bf9c85dd80190334df2
TakayaShirai <s78taka03@gmail.com>
2025-04-10 23:54:32 -0700

    [swift] Update 88. Merge Sorted Array.

--- swift/88_merge_sorted_array.swift
+++ swift/88_merge_sorted_array.swift
@@ -1,25 +1,22 @@
 class Solution {
   func merge(_ nums1: inout [Int], _ m: Int, _ nums2: [Int], _ n: Int) {
-    var last: Int = m + n - 1
-    var index1: Int = m - 1
-    var index2: Int = n - 1
+    var fillIdx: Int = n + m - 1
+    var nums1Idx: Int = m - 1
+    var nums2Idx: Int = n - 1
 
-    while index1 >= 0 && index2 >= 0 {
-      if nums1[index1] > nums2[index2] {
-        nums1[last] = nums1[index1]
-        index1 -= 1
+    while nums1Idx >= 0 || nums2Idx >= 0 {
+      let curNums1Num: Int = nums1Idx >= 0 ? nums1[nums1Idx] : Int.min
+      let curNums2Num: Int = nums2Idx >= 0 ? nums2[nums2Idx] : Int.min
+
+      if curNums1Num > curNums2Num {
+        nums1[fillIdx] = curNums1Num
+        nums1Idx -= 1
       } else {
-        nums1[last] = nums2[index2]
-        index2 -= 1
+        nums1[fillIdx] = curNums2Num
+        nums2Idx -= 1
       }
 
-      last -= 1
-    }
-
-    while index2 >= 0 {
-      nums1[last] = nums2[index2]
-      index2 -= 1
-      last -= 1
+      fillIdx -= 1
     }
   }
 }
