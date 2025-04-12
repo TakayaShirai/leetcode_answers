ea90cd3d35dc4f576363fa2caff9cb404a6f3cd0
TakayaShirai <s78taka03@gmail.com>
2025-04-11 19:19:50 -0700

    [swift] Update 26. Remove Duplicates from Sorted Array.

--- swift/26_remove_duplicates_from_sorted_array.swift
+++ swift/26_remove_duplicates_from_sorted_array.swift
@@ -1,44 +1,22 @@
 class Solution {
   func removeDuplicates(_ nums: inout [Int]) -> Int {
-    let allowableDuplicateCnt: Int = 1
-    var curIdx: Int = 0
+    var fillInIdx: Int = 0
+    var seenNums = Set<Int>()
+    var deletionCnt: Int = 0
 
-    for num in nums {
-      if curIdx < allowableDuplicateCnt || nums[curIdx - allowableDuplicateCnt] != num {
-        nums[curIdx] = num
-        curIdx += 1
+    for curIdx in 0..<nums.count {
+      let curNum: Int = nums[curIdx]
+
+      guard !seenNums.contains(curNum) else {
+        deletionCnt += 1
+        continue
       }
+
+      nums[fillInIdx] = curNum
+      fillInIdx += 1
+      seenNums.insert(curNum)
     }
 
-    return curIdx
-
-    // O(n^2)
-    // var curIdx: Int = 0
-
-    // while curIdx < nums.count {
-    //     if curIdx != 0 && nums[curIdx] == nums[curIdx - 1] {
-    //         nums.remove(at: curIdx)
-    //     } else {
-    //         curIdx += 1
-    //     }
-    // }
-
-    // return nums.count
-
-    // O(n)
-    // var left: Int = 0
-
-    // for right in 0..<nums.count {
-    //     if nums[left] != nums[right] {
-    //         left += 1
-    //         nums[left] = nums[right]
-    //     }
-    // }
-
-    // for _ in 0..<(nums.count - left - 1) {
-    //     nums.removeLast()
-    // }
-
-    // return nums.count
+    return nums.count - deletionCnt
   }
 }
