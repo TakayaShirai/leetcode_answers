e50487b64c3af319fdcaded42998bf9830425d71
TakayaShirai <s78taka03@gmail.com>
2025-04-11 21:20:27 -0700

    [swift] Update 80. Remove Duplicates from Sorted Array II.

--- swift/80_remove_duplicates_from_sorted_array_II.swift
+++ swift/80_remove_duplicates_from_sorted_array_II.swift
@@ -1,42 +1,29 @@
 class Solution {
   func removeDuplicates(_ nums: inout [Int]) -> Int {
-    let allowableDuplicateCnt: Int = 2
+    let maxCnt: Int = 2
 
-    var curIdx: Int = 0
+    var curCnt: Int = 0
+    var curNum: Int = nums[0]
+    var fillInIdx: Int = 0
+    var deletionCnt: Int = 0
 
-    for num in nums {
-      if curIdx < allowableDuplicateCnt || nums[curIdx - allowableDuplicateCnt] != num {
-        nums[curIdx] = num
-        curIdx += 1
+    for curIdx in 0..<nums.count {
+      if nums[curIdx] != curNum {
+        curNum = nums[curIdx]
+        curCnt = 0
       }
+
+      curCnt += 1
+
+      guard curCnt <= maxCnt else {
+        deletionCnt += 1
+        continue
+      }
+
+      nums[fillInIdx] = curNum
+      fillInIdx += 1
     }
 
-    return curIdx
-
-    // var prevNum: Int = nums[0]
-    // var curNumCnt: Int = 0
-    // var curIdx: Int = 0
-    // var originalNumsCnt: Int = nums.count
-    // var deleteCnt: Int = 0
-
-    // while curIdx < nums.count {
-    //     let curNum: Int = nums[curIdx]
-
-    //     if curNum == prevNum {
-    //         if curNumCnt == 2 {
-    //             deleteCnt += 1
-    //             nums.remove(at: curIdx)
-    //         } else {
-    //             curNumCnt += 1
-    //             curIdx += 1
-    //         }
-    //     } else {
-    //         prevNum = curNum
-    //         curIdx += 1
-    //         curNumCnt = 1
-    //     }
-    // }
-
-    // return originalNumsCnt - deleteCnt
+    return nums.count - deletionCnt
   }
 }
