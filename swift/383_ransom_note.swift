1478375ad65d817d404eaae571ad363bc90f79ed
TakayaShirai <s78taka03@gmail.com>
2025-04-16 14:11:59 -0700

    [swift] Update 383. Ransom Note.

--- swift/383_ransom_note.swift
+++ swift/383_ransom_note.swift
@@ -1,29 +1,23 @@
 class Solution {
   func canConstruct(_ ransomNote: String, _ magazine: String) -> Bool {
-    var magCharCntMap: [Character: Int] = createCharCntMap(magazine)
+    var magazineCharCounter: [Character: Int] = createCharCounter(of: magazine)
 
     for char in ransomNote {
-      if magCharCntMap[char] != nil {
-        if magCharCntMap[char] == 0 {
-          return false
-        } else {
-          magCharCntMap[char]! -= 1
-        }
-      } else {
-        return false
-      }
+      let charCount = magazineCharCounter[char, default: 0]
+      guard charCount > 0 else { return false }
+      magazineCharCounter[char, default: 0] -= 1
     }
 
     return true
   }
 
-  private func createCharCntMap(_ str: String) -> [Character: Int] {
-    var map: [Character: Int] = [:]
+  private func createCharCounter(of str: String) -> [Character: Int] {
+    var charCounter: [Character: Int] = [:]
 
     for char in str {
-      map[char, default: 0] += 1
+      charCounter[char, default: 0] += 1
     }
 
-    return map
+    return charCounter
   }
 }
