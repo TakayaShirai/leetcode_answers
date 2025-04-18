5216aa986d810382d4154ef4de92d05dfe15b441
TakayaShirai <s78taka03@gmail.com>
2025-04-17 18:54:11 -0700

    [swift] Update 35. Search Insert Position.

--- swift/35_search_insert_position.swift
+++ swift/35_search_insert_position.swift
@@ -4,14 +4,14 @@ class Solution {
     var right: Int = nums.count - 1
 
     while left <= right {
-      let mid = left + (right - left) / 2
+      let middle: Int = left + (right - left) / 2
 
-      if nums[mid] == target {
-        return mid
-      } else if nums[mid] < target {
-        left = mid + 1
+      if nums[middle] == target {
+        return middle
+      } else if nums[middle] > target {
+        right = middle - 1
       } else {
-        right = mid - 1
+        left = middle + 1
       }
     }
 
