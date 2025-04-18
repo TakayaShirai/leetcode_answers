80ea950311ed6a5e6921e18fd0036f4f6c49f9f1
TakayaShirai <s78taka03@gmail.com>
2025-04-17 19:05:08 -0700

    [swift] Update 112. Path Sum.

--- swift/112_path_sum.swift
+++ swift/112_path_sum.swift
@@ -15,11 +15,11 @@ class Solution {
   func hasPathSum(_ root: TreeNode?, _ targetSum: Int) -> Bool {
     guard let root = root else { return false }
 
-    if root.left == nil && root.right == nil {
-      return root.val == targetSum
+    if root.left == nil && root.right == nil && root.val == targetSum {
+      return true
     }
 
-    let remainingSum = targetSum - root.val
-    return hasPathSum(root.left, remainingSum) || hasPathSum(root.right, remainingSum)
+    return hasPathSum(root.left, targetSum - root.val)
+      || hasPathSum(root.right, targetSum - root.val)
   }
 }
