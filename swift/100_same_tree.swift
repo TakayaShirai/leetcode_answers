656df062069af51a106bc4a9ef29b36e6567c85c
TakayaShirai <s78taka03@gmail.com>
2025-04-21 21:53:57 -0700

    [swift] Update 100. Same Tree.

--- swift/100_same_tree.swift
+++ swift/100_same_tree.swift
@@ -13,48 +13,10 @@
 /// }
 class Solution {
   func isSameTree(_ p: TreeNode?, _ q: TreeNode?) -> Bool {
-    return dfsIsSameTree(p, q)
-    // return bfsIsSameTree(p, q)
-  }
-
-  func dfsIsSameTree(_ p: TreeNode?, _ q: TreeNode?) -> Bool {
-    if p == nil && q == nil {
-      return true
-    }
-
-    let isLeftSame = dfsIsSameTree(p?.left, q?.left)
-    let isRightSame = dfsIsSameTree(p?.right, q?.right)
-
-    if p?.val == q?.val && isLeftSame && isRightSame {
-      return true
-    }
-
-    return false
-  }
-
-  func bfsIsSameTree(_ p: TreeNode?, _ q: TreeNode?) -> Bool {
-    var queueP: [TreeNode?] = [p]
-    var queueQ: [TreeNode?] = [q]
-
-    while !queueP.isEmpty && !queueQ.isEmpty {
-      var curP = queueP.removeFirst()
-      var curQ = queueQ.removeFirst()
-
-      if curP?.val != curQ?.val {
-        return false
-      }
-
-      if curP != nil {
-        queueP.append(curP!.left)
-        queueP.append(curP!.right)
-      }
-
-      if curQ != nil {
-        queueQ.append(curQ!.left)
-        queueQ.append(curQ!.right)
-      }
-    }
-
-    return true
+    guard !(p == nil && q == nil) else { return true }
+    guard let strongP = p, let strongQ = q else { return false }
+    let isLeftSame: Bool = isSameTree(strongP.left, strongQ.left)
+    let isRightSame: Bool = isSameTree(strongP.right, strongQ.right)
+    return isLeftSame && isRightSame && strongP.val == strongQ.val
   }
 }
