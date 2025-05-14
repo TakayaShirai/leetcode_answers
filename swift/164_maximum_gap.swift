08df7514dcd7cf6ce895e3b0e6412a97ba63c866
TakayaShirai <s78taka03@gmail.com>
2025-05-15 00:15:08 +0900

    [swift] Update 164. Maximum Gap.

--- swift/164_maximum_gap.swift
+++ swift/164_maximum_gap.swift
@@ -1,51 +1,122 @@
+import XCTest
+
+/// An enum to represent the errors that could occur in the method maximumGap.
+enum MaximumGapError: Error {
+  case invalidInputSizeError
+}
+
 class Solution {
-  func maximumGap(_ nums: [Int]) -> Int {
-    guard nums.count >= 2 else { return 0 }
+  /// Calculates the maximum gap between successive elements in the input array.
+  ///
+  /// - Parameter nums: An array of integers.
+  /// - Returns: The maximum gap between successive elements.
+  /// - Throws: `MaximumGapError.invalidInputSizeError` if the input array has fewer than two elements.
+  ///           `MaximumGapError.invalidInputContentError` if the array contains invalid content.
+  func findMaximumGap(in nums: [Int]) throws -> Int {
+    // Ensure the input contains at least two elements to form a gap.
+    guard nums.count >= 2 else { throw MaximumGapError.invalidInputSizeError }
+    let minNum = nums.min()!  // Safe to force-unwrap .min() here, as 'nums.count >= 2' ensures 'nums' is non-empty.
+    let maxNum = nums.max()!  // Safe to force-unwrap .max() here, as 'nums.count >= 2' ensures 'nums' is non-empty.
 
-    let minMaxNums: (min: Int, max: Int) = findMinMax(from: nums)
-    let minNum: Int = minMaxNums.min
-    let maxNum: Int = minMaxNums.max
+    // If all numbers are the same, the maximum gap is 0.
+    guard maxNum != minNum else { return 0 }
 
-    guard minNum != maxNum else { return 0 }
+    // Calculate the minimum possible maximum gap.
+    // This is the smallest maximum gap that must exist between sorted elements,
+    // assuming they are uniformely distributed.
+    // We use this value to define the size of the following buckets.
+    let minPossibleMaxGap: Int = max(1, (maxNum - minNum) / (nums.count - 1))
 
-    let numsLen: Int = nums.count
+    // Calculate the number of buckets needed to span the entire range from minNum to maxNum.
+    let bucketsCnt: Int = (maxNum - minNum) / minPossibleMaxGap + 1
 
-    let bucketSize: Int = max(1, (maxNum - minNum) / (numsLen - 1))
-    let bucketCnt: Int = (maxNum - minNum) / bucketSize + 1
+    // Initialize arrays to track the min and max value in each bucket.
 
-    var minBuckets: [Int] = Array(repeating: Int.max, count: bucketCnt)
-    var maxBuckets: [Int] = Array(repeating: Int.min, count: bucketCnt)
-    var filledBuckets: [Bool] = Array(repeating: false, count: bucketCnt)
+    // The maximum gap will not be found within a bucket because the range of a bucket is set small.
+    // Instead, the max gap must occur between two buckets (between max of one bucket and min of the next).
+    // So by storing only the min and max of each non-empty bucket, we can compute the max gap in O(n) time.
+    var minNumsBuckets: [Int?] = Array(repeating: nil, count: bucketsCnt)
+    var maxNumsBuckets: [Int?] = Array(repeating: nil, count: bucketsCnt)
 
-    for num in nums {
-      let bucketIdx: Int = (num - minNum) / bucketSize
-      minBuckets[bucketIdx] = min(minBuckets[bucketIdx], num)
-      maxBuckets[bucketIdx] = max(maxBuckets[bucketIdx], num)
-      filledBuckets[bucketIdx] = true
+    // Distribute the numbers into their respective buckets and update the min and max values.
+    for curNum in nums {
+      let bucketIdx: Int = (curNum - minNum) / minPossibleMaxGap
+
+      if let curMin = minNumsBuckets[bucketIdx], let curMax = maxNumsBuckets[bucketIdx] {
+        minNumsBuckets[bucketIdx] = min(curMin, curNum)
+        maxNumsBuckets[bucketIdx] = max(curMax, curNum)
+      } else {
+        minNumsBuckets[bucketIdx] = curNum
+        maxNumsBuckets[bucketIdx] = curNum
+      }
     }
 
-    var maxGap: Int = 0
+    var curMaxGap: Int = 0
     var prevMax: Int = minNum
 
-    for curBucketIdx in 0..<bucketCnt {
-      guard filledBuckets[curBucketIdx] else { continue }
+    // Compute the maximum gap by comparing the minimum value of the current non-empty bucket
+    // with the maximum value of the previous non-empty bucket.
+    for curBucketIdx in 0..<bucketsCnt {
+      guard let curMin = minNumsBuckets[curBucketIdx], let curMax = maxNumsBuckets[curBucketIdx]
+      else {
+        continue
+      }
 
-      maxGap = max(maxGap, minBuckets[curBucketIdx] - prevMax)
-      prevMax = maxBuckets[curBucketIdx]
+      curMaxGap = max(curMaxGap, curMin - prevMax)
+      prevMax = curMax
     }
 
-    return maxGap
+    return curMaxGap
+  }
+}
+
+final class MaximumGap: XCTestCase {
+
+  var solution: Solution!
+
+  override func setUp() {
+    super.setUp()
+    solution = Solution()
   }
 
-  private func findMinMax(from nums: [Int]) -> (min: Int, max: Int) {
-    var maxNum: Int = Int.min
-    var minNum: Int = Int.max
+  override func tearDown() {
+    solution = nil
+    super.tearDown()
+  }
 
-    for num in nums {
-      minNum = min(num, minNum)
-      maxNum = max(num, maxNum)
+  func testFindMaximamGapWhenGivenDifferentInputNumbers() {
+    do {
+      let inputNums: [Int] = [2, 1, 5, 6, 12]
+      let expectedRes: Int = 6
+      let result: Int = try solution.findMaximumGap(in: inputNums)
+      XCTAssertEqual(expectedRes, result, "Expected \(expectedRes), but got \(result).")
+    } catch {
+      XCTFail("Unexpected error occurred: \(error).")
     }
+  }
 
-    return (min: minNum, max: maxNum)
+  func testFindMaximamGapWhenGivenSameInputNumbers() {
+    do {
+      let inputNums: [Int] = [1, 1, 1]
+      let expectedRes: Int = 0
+      let result: Int = try solution.findMaximumGap(in: inputNums)
+      XCTAssertEqual(expectedRes, result, "Expected \(expectedRes), but got \(result).")
+    } catch {
+      XCTFail("Unexpected error occurred: \(error).")
+    }
+  }
+
+  func testFindMaximamGapWhenGivenInvalidInputSizeNumbers() {
+    let inputNums: [Int] = [1]
+
+    XCTAssertThrowsError(try solution.findMaximumGap(in: inputNums)) { error in
+      if let maximumGapError = error as? MaximumGapError {
+        XCTAssertEqual(
+          maximumGapError, .invalidInputSizeError,
+          "Expected \(MaximumGapError.invalidInputSizeError), but got \(error)")
+      } else {
+        XCTFail("Unexpected error occurred: \(error).")
+      }
+    }
   }
 }
