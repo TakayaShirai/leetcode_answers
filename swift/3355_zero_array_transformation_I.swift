bc12c5def055c4afdf9de4fedda2c248800a563d
TakayaShirai <s78taka03@gmail.com>
2025-05-20 18:28:39 +0900

    [swift] Add 3355. Zero Array Transformation I.

---
+++ swift/3355_zero_array_transformation_I.swift
@@ -0,0 +1,23 @@
+class Solution {
+  func isZeroArray(_ nums: [Int], _ queries: [[Int]]) -> Bool {
+    var diffArray: [Int] = Array(repeating: 0, count: nums.count)
+
+    for query in queries {
+      let left: Int = query[0]
+      let right: Int = query[1]
+
+      diffArray[left] += 1
+      guard right + 1 < diffArray.count else { continue }
+      diffArray[right + 1] -= 1 
+    }
+
+    var curDecrement: Int = 0
+
+    for (idx, diff) in diffArray.enumerated() {
+      curDecrement += diff
+      guard curDecrement >= nums[idx] else { return false }
+    }
+
+    return true
+  }
+}
