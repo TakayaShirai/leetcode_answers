47642f58bcb63c1d57de2c9b20a7fd5d79708f36
TakayaShirai <s78taka03@gmail.com>
2025-05-22 19:35:46 +0900

    [swift] Update 150. Evaluate Reverse Polish Notation.

--- swift/150_evaluate_reverse_polish_notation.swift
+++ swift/150_evaluate_reverse_polish_notation.swift
@@ -1,12 +1,13 @@
 class Solution {
   func evalRPN(_ tokens: [String]) -> Int {
     var numsStack: [Int] = []
+    let minimumRequiredNumsCnt: Int = 2
 
     for token in tokens {
       if let num = Int(token) {
         numsStack.append(num)
       } else {
-        guard numsStack.count >= 2 else { return -1 }
+        guard numsStack.count >= minimumRequiredNumsCnt else { return -1 }
         let rightNum: Int = numsStack.removeLast()
         let leftNum: Int = numsStack.removeLast()
         switch token {
